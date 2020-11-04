24e4784656fcb438a549ae75e29e453242cb54cb
alexandrajulius <alexandra.julius@gmail.com>
2020-11-04 19:49:50 +0100

    uses generators in unit tests

--- tests/unit/RailTest.php
+++ tests/unit/RailTest.php
@@ -4,35 +4,86 @@ declare(strict_types=1);
 
 namespace tests\unit;
 
+use Generator;
 use Rail;
 use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 
 final class RailTest extends TestCase
 {
-    public function testCanBeInstantiated()
+    public function testCanBeInstantiated(): void
     {
         $example = new Rail();
         Assert::assertInstanceOf(Rail::class, $example);
     }
-
-    function testDryRun()
+    
+    /**
+     * @dataProvider provideRailData
+     */
+    public function testConvertRailsToMatrixIndices(int $numberOfLetters, int $numberOfRails, array $expectedMatrixIndices): void
     {
-        $actualArray = (new Rail())->getRails(10,4);
+        $rail = new Rail();
 
-        $expectedArray = [
-            0 => 0,
-            1 => 1,
-            2 => 2,
-            3 => 3,
-            4 => 2,
-            5 => 1,
-            6 => 0,
-            7 => 1,
-            8 => 2,
-            9 => 3
+        $actualMatrixIndices = $rail->getRails($numberOfLetters, $numberOfRails);
+
+        self::assertEquals($expectedMatrixIndices, $actualMatrixIndices);
+    }
+
+    public function provideRailData(): Generator
+    {
+        yield 'MatrixIndices for 4 letters and 10 rails' => [
+            'numberOfLetters' => 4,
+            'numberOfRails' => 10,
+            'expectedMatrixIndices' => [
+                0 => 0,
+                1 => 1,
+                2 => 2,
+                3 => 3
+            ]
         ];
 
-        $this->assertEquals($expectedArray, $actualArray, 'Number of Rails do not match.');
+        yield 'MatrixIndices for 10 letters and 4 rails' => [
+            'numberOfLetters' => 10,
+            'numberOfRails' => 4,
+            'expectedMatrixIndices' => [
+                0 => 0,
+                1 => 1,
+                2 => 2,
+                3 => 3,
+                4 => 2,
+                5 => 1,
+                6 => 0,
+                7 => 1,
+                8 => 2,
+                9 => 3
+            ]
+        ];
+
+        yield 'MatrixIndices for 20 letters and 3 rails' => [
+            'numberOfLetters' => 20,
+            'numberOfRails' => 3,
+            'expectedMatrixIndices' => [
+                0 => 0,
+                1 => 1,
+                2 => 2,
+                3 => 1,
+                4 => 0,
+                5 => 1,
+                6 => 2,
+                7 => 1,
+                8 => 0,
+                9 => 1,
+                10 => 2,
+                11 => 1,
+                12 => 0,
+                13 => 1,
+                14 => 2,
+                15 => 1,
+                16 => 0,
+                17 => 1,
+                18 => 2,
+                19 => 1
+            ]
+        ];
     }
 }
